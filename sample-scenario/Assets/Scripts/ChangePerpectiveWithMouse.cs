735fe58a54c1eb6d61df1aadd638963dd8e80cf1
allaa-aa1404600 <aa1404600@qu.edu.qa>
2018-09-27 16:01:03 +0300

    updated with Class B fire scene

---
+++ sample-scenario/Assets/Scripts/ChangePerpectiveWithMouse.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangePerpectiveWithMouse : MonoBehaviour {
+    float horizontalSpeed = 2.0f;
+    float verticalSpeed = 2.0f;
+
+    void Update()
+    {
+        // Get the mouse delta. This is not in the range -1...1
+        float h = horizontalSpeed * Input.GetAxis("Mouse X");
+        float v = verticalSpeed * Input.GetAxis("Mouse Y");
+
+        transform.Rotate(v, h, 0);
+    }
+}
---
+++ sample-scenario/Assets/Scripts/DistanceToFire.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DistanceToFire : MonoBehaviour {
+
+    public Transform other;
+    public Text distanceText;
+
+	// Use this for initialization
+	void Start () {
+        
+    	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (other)
+        {
+            float dist = Vector3.Distance(other.position, transform.position);
+            //print("Distance to other: " + dist);
+            distanceText.text = "Distance from the fire: " + dist.ToString("#.##")+"m";
+        }
+		
+	}
+}
